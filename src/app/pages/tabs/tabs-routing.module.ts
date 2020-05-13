42d34e96998a531ebde2ab2ac4d85f36fa073673
devonroc <devonrock_@hotmail.com>
2020-05-13 21:14:32 +0000

    camera fuction

---
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,84 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+
+    //Here are the routes that redirects to the modules of each page, also are the paths that redirect to
+    //the subpages if are available
+    {
+        path: 'tabs',
+        component: TabsPage,
+        children: [
+            {
+                path: 'episodes',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('../episodes/episodes.module').then(m => m.EpisodesPageModule)
+                    },
+                    {
+                        path: ':id',
+                        loadChildren: () => import('../episode-details/episode-details.module').then(m => m.EpisodeDetailsPageModule)
+                    }
+                ]
+            },
+            {
+                path: 'characters',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('../characters/characters.module').then(m => m.CharactersPageModule)
+                    },
+                    {
+                        path: ':id',
+                        loadChildren: () => import('../character-details/character-details.module').then(m => m.CharacterDetailsPageModule)
+                    }
+                ]
+            },
+            {
+                path: 'quotes',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('../quotes/quotes.module').then(m => m.QuotesPageModule)
+                    },
+                    {
+                        path: ':id',
+                        loadChildren: () => import('../quote-details/quote-details.module').then(m => m.QuoteDetailsPageModule)
+                    }
+                ]
+            },
+            {
+                path: 'deaths',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('../deaths/deaths.module').then(m => m.DeathsPageModule)
+                    }
+                ]
+            },
+
+                {
+                path: 'camera',
+                children: [
+                    {
+                        path: '',
+                        loadChildren: () => import('../camera/camera.module').then(m => m.CameraPageModule)
+                    },
+                   
+                ]
+            },
+        ]
+    },
+
+    {
+        path: '',
+        redirectTo: '/tabs/episodes',
+        pathMatch: 'full'
+    }
+]; @NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class TabsPageRoutingModule { }
